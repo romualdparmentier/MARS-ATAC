8adc9521dca0ff625eef0f6bdfd0becdafdba48a
Romuald Parmentier <romualdparmentier@orange.fr>
2021-09-09 14:28:12 +0200

    Fixing input and output path
    1. Input path now points to the last experience of bam downsampling
    2. Ouput are now generated in a folder named by the date (YY_mm_dd_hh_min)nhb

--- bin/ATAC_peak_calling.sh
+++ bin/ATAC_peak_calling.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-cd /media/romuald/TOSHIBA_EXT/Romuald_NGS/MARS-ATAC/exp/ATAC_bam_downsampling/2021_01_01
+cd /home/romuald/MARS-ATAC/exp/ATAC_bam_downsampling
+cd $(ls -ltr |grep ^d |tail -1 | awk '{print $9}') # chande directory to the last folder created ($9 is the 9th field of the file metadata >> i.e complete file name)
 
 source ~/python_env/MACS2/bin/activate
+
+dt=$(date '+%Y_%m_%d_%Hh_%Mmin/')
+mkdir /home/romuald/MARS-ATAC/exp/ATAC_peak_calling/$dt
 
 for file in in $(ls -t | grep cut_downsampled)
 
 do
   name=$(basename -- $file | head -c 3)
-  dir=/media/romuald/TOSHIBA_EXT/Romuald_NGS/MARS-ATAC/exp/ATAC_peak_calling/2021_01_01/
+  dir=/home/romuald/MARS-ATAC/exp/ATAC_peak_calling/$dt
   macs2 callpeak -t $file -n $name\_cut --outdir $dir -f BAMPE -g hs -B --broad --broad-cutoff 0.1
 done
